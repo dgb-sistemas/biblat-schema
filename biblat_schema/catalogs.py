28430a8e26c5eeb410b76e04506b643d5cf500fc
Mario Gama Mora <mgamam@dgb.unam.mx>
2018-06-06 16:46:53 -0500

    Correccion al esquema de catalogos para la base documental tk#17 de biblat-schema

--- biblat_schema/catalogs.py
+++ biblat_schema/catalogs.py
@@ -2,15 +2,19 @@
 from mongoengine import (
     Document,
     StringField,
-    IntField,
-    ReferenceField
+    EmbeddedDocument,
+    EmbeddedDocumentField
 )
 
 
-class PaisRevista(Document):
-    """Esquema de catalogo pais revista"""
+class I18NField(EmbeddedDocument):
+    es = StringField()
+    en = StringField()
+
+
+class Pais(Document):
+    """Esquema de catalogo pais"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_pais = IntField(required=True)
     alpha3 = StringField(min_length=3, max_length=3, required=True)
     nombre = StringField(max_length=250, required=True)
 
@@ -18,7 +22,6 @@ class PaisRevista(Document):
 class Idioma(Document):
     """Esquema de catalogo idioma"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_idioma = IntField(required=True)
     codigo = StringField(min_length=3, max_length=3, required=True)
     nombre = StringField(max_length=250, required=True)
 
@@ -26,7 +29,6 @@ class Idioma(Document):
 class TipoDocumento(Document):
     """Esquema de catalogo tipo documento"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_tipo_documento = IntField(required=True)
     nombre = StringField(max_length=150, required=True)
     descripcion = StringField(max_length=700, required=True)
 
@@ -34,7 +36,6 @@ class TipoDocumento(Document):
 class EnfoqueDocumento(Document):
     """Esquema de catalogo enfoque documento"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_enfoque = IntField(required=True)
     nombre = StringField(max_length=150, required=True)
     descripcion = StringField(max_length=700, required=True)
 
@@ -42,29 +43,24 @@ class EnfoqueDocumento(Document):
 class Disciplina(Document):
     """Esquema de catalogo disciplina"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_disciplina = IntField(required=True)
-    nombre = StringField(max_length=150, required=True)
+    nombre = EmbeddedDocumentField(I18NField)
 
 
 class SubDisciplina(Document):
     """Esquema de catalogo subdisciplina"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_disciplina = ReferenceField(Disciplina)
-    nombre = StringField(max_length=150, required=True)
+    nombre = EmbeddedDocumentField(I18NField)
 
 
 class NombreGeografico(Document):
     """Esquema de catalogo nombre geografico"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_nombre_geografico = IntField(required=True)
-    nombre_espaniol = StringField(max_length=250, required=True)
-    nombre_ingles = StringField(max_length=250, required=True)
+    nombre = EmbeddedDocumentField(I18NField)
 
 
 class DisciplinaRevista(Document):
     """Esquema de catalogo disciplina revista"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_disciplina = IntField(required=True)
     base = StringField(max_length=10, required=True)
     nombre = StringField(max_length=150, required=True)
 
@@ -72,7 +68,6 @@ class DisciplinaRevista(Document):
 class LicenciaCC(Document):
     """Esquema de catalogo licencia"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_licencia = IntField(required=True)
     tipo = StringField(max_length=6, required=True)
     descripcion = StringField(max_length=150, required=True)
 
@@ -80,6 +75,5 @@ class LicenciaCC(Document):
 class SherpaRomeo(Document):
     """Esquema de catalogo sherpa romeo"""
     _id = StringField(max_length=32, primary_key=True, required=True)
-    id_sherpa_romeo = IntField(required=True)
     color = StringField(max_length=10, required=True)
     politica = StringField(max_length=70, required=True)
