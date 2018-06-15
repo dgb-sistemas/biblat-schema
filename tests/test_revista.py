81dc2ea87bae8a28a847301b9496a74c54d912d4
Arturo Rendón Cruz <arendon@dgb.unam.mx>
2018-06-15 11:41:51 -0500

    Corrección de pruebas unitarias para revista

--- tests/test_revista.py
+++ tests/test_revista.py
@@ -1,22 +1,38 @@
 # coding: utf-8
 from datetime import datetime
 from biblat_schema.models import Revista
+from biblat_schema.catalogs import DisciplinaRevista
 from .base import BaseTestCase
 
 
 class TestJournalModel(BaseTestCase):
     model_class_to_delete = [Revista]
 
+    def _crear_disciplina_revista_prueba(self):
+        disciplina_id = self.generate_uuid_32_string()
+        disciplina_data = {
+            '_id': disciplina_id,
+            'nombre': {
+                'es': u'Antropología',
+                'en': 'Anthropology'
+            }
+        }
+        disciplina_doc = DisciplinaRevista(**disciplina_data)
+        return disciplina_doc
+
     def test_solo_campos_requeridos(self):
+        # Disciplina revista
+        disciplina_doc = self._crear_disciplina_revista_prueba()
+
         # Datos
-        _id = self.generate_uuid_32_string()
+        revista_id = self.generate_uuid_32_string()
         revista_data = {
-            '_id': _id,
+            '_id': revista_id,
             'base_datos': 'CLA01',
             'titulo': u'Estudios de cultura náhuatl',
             'issn': '0071-1675',
             'pais': 'MX',
-            'disciplina': u'Antropología',
+            'disciplina': disciplina_doc,
             'fecha_creacion': datetime.now(),
             'fecha_actualizacion': datetime.now(),
         }
@@ -26,7 +42,7 @@ class TestJournalModel(BaseTestCase):
         revista_doc.save()
 
         # Comprobamos
-        self.assertEqual(_id, revista_doc._id)
+        self.assertEqual(revista_id, revista_doc._id)
         self.assertEqual(revista_data['base_datos'], revista_doc.base_datos)
         self.assertEqual(revista_data['titulo'], revista_doc.titulo)
         self.assertEqual(revista_data['issn'], revista_doc.issn)
