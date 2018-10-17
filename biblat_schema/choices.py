74e2e456a79685f6d68525521b40941255ea5705
Arturo Rendón Cruz <arendon@dgb.unam.mx>
2018-10-17 10:59:44 -0500

    Descriptores homologados

--- biblat_schema/choices.py
+++ biblat_schema/choices.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 FREQUENCY = [
-    ('M', 'Mensual'),
-    ('B', 'Bimestral (cada dos meses)'),
-    ('Q', 'Trimestral'),
-    ('T', 'Tres veces por año'),
-    ('F', 'Semestral (dos veces por año)'),
-    ('A', 'Anual'),
+    ('M', 'Mensual (doce veces al año)'),
+    ('B', 'Bimestral (seis veces al año)'),
+    ('T', 'Trimestral (cuatro veces al año)'),
+    ('C', 'Cuatrimestral (tres veces al año)'),
+    ('S', 'Semestral (dos veces por año)'),
+    ('A', 'Anual (una vez al año)'),
     ('K', 'Irregular'),
 ]
